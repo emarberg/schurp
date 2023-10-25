c50ae430c6cfd6a5408ad37cae124448515daad1
Eric Marberg <eric.marberg@gmail.com>
2023-10-25 12:30:42 +0800

    oriented kromatic

--- tests/stable/test_kromatic.py
+++ tests/stable/test_kromatic.py
@@ -51,17 +51,26 @@ def test_G_expansion(nvars=3, nverts=3):
 
 
 def test_G_oriented_expansion(nvars=3, n=5, r=5):
+    seen = set()
     for nn in range(1, n + 1):
         for rr in range(1, r + 1):
             v, e = natural_unit_iterval_order_incompability_graph(nn, rr)
+            
+            v, e = tuple(sorted(v)), tuple(sorted(e))
+            if (v, e) not in seen:
+                seen.add((v, e))
+            else:
+                continue
+            
             icf = is_claw_free(v, e)
             try:
                 f = oriented_kromatic(nvars, v, e)
                 exp = oriented_expander(mn_G_expansion, f)
                 pos = exp.is_nonnegative()
-                # print(pos, exp)
-                # print()
+                print(v, e, icf, pos, exp)
+                print()
                 assert (not icf) or pos
             except:
                 print(v, e, icf)
                 print('pass\n')
+                input('')
