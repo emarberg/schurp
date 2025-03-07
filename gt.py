7ed4b464d3e52710aba82cb7969bf41a54f8e26b
Eric Marberg <eric.marberg@gmail.com>
2025-03-07 09:05:13 +0800

    gelfand tsetlin patterns blank files

---
+++ gt.py
@@ -0,0 +1,42 @@
+from schubert import X
+from stable.partitions import Partitions
+
+
+class GTPattern:
+
+    def __init__(self, array, enhancements=None):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __hash__(self):
+        pass
+
+    def __eq__(self):
+        pass
+
+    def weight_enhanced(self):
+        pass
+
+    def weight_strict(self):
+        pass
+
+    def weight(self):
+        pass
+
+    def get(self, i, j):
+        pass
+
+    def __len__(self):
+        pass
+
+    def row(self, i):
+        pass
+
+    def is_strict(self):
+        pass
+
+    @classmethod
+    def all(cls, n, strict=False, enhanced=False):
+        pass
